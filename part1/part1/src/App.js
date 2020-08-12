7b9aa7189f2f7d93430321d0f98f5e18da4b7a51
Jugveer Sandher <jugveersandher@MacBook-Pro.local>
2020-08-12 10:38:23 -0700

    1.3 - 1.5

--- part1/part1/src/App.js
+++ part1/part1/src/App.js
@@ -5,15 +5,36 @@ import Footer from './Footer';
 
 const App = () => {
     
+    const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  var array = (course);
   
     return (
       <div>
 
-        <Header />
-        
+        {/* <Header />
         <Content />
+        <Footer /> */}
 
-        <Footer />
+        <Header course={course[0]} />
+        <Content parts={course[1]} />
+        <Footer parts={course[2]} />
 
       </div>
     )
